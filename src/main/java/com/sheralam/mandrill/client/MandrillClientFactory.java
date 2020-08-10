4ed6e7a79177b04382b878a6178ac49fe5be35b9
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-10 23:36:40 +0600

    Implement Mandrill Client Factory

---
+++ src/main/java/com/sheralam/mandrill/client/MandrillClientFactory.java
@@ -0,0 +1,48 @@
+package com.sheralam.mandrill.client;
+
+import lombok.NonNull;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
+
+/**
+ * MandrillClientFactory Provides various different ways of creating  {@link MandrillClient}.<br/>
+ * ApiKey is the Must,<br/><br/> however, should we need to override default {@link RestTemplate} provided by the {@link RestTemplateSupplier},<br/>
+ * We can provide either an Object of {@link RestTemplate} or {@link Supplier} of {@link RestTemplate}
+ */
+public class MandrillClientFactory {
+    final static private Map<String, MandrillClient> clientsMap = new ConcurrentHashMap<>();
+
+    /**
+     *
+     * @param apiKey - mandrill api key
+     * @return MandrillClient with default setup
+     */
+    public static synchronized MandrillClient getClientForApiKey(@NonNull String apiKey) {
+        return clientsMap.computeIfAbsent(apiKey, MandrillClient::new);
+    }
+
+    /**
+     *
+     * @param apiKey - mandrill api key
+     * @param restTemplateSupplier supplies RestTemplate that overrides default
+     * @return MandrillClient with provided RestTemplate
+     */
+    public static synchronized MandrillClient getClientForApiKey(@NonNull String apiKey, @NonNull Supplier<RestTemplate> restTemplateSupplier) {
+        return clientsMap.computeIfAbsent(apiKey, key -> new MandrillClient(key, restTemplateSupplier));
+    }
+
+
+    /**
+     *
+     * @param apiKey - mandrill api key
+     * @param restTemplate  that overrides default
+     * @return MandrillClient with provided RestTemplate
+     */
+    public static synchronized MandrillClient getClientForApiKey(@NonNull String apiKey, @NonNull RestTemplate restTemplate) {
+        return clientsMap.computeIfAbsent(apiKey, key -> new MandrillClient(key, restTemplate));
+    }
+}
