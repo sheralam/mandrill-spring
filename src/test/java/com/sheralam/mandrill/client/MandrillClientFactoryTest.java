e05f007f968b33116b4dec28f5d2230e58686e4a
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-10 23:34:46 +0600

    Add Mandrill Client Factory Test

---
+++ src/test/java/com/sheralam/mandrill/client/MandrillClientFactoryTest.java
@@ -0,0 +1,36 @@
+package com.sheralam.mandrill.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class MandrillClientFactoryTest {
+    String testApiKey = "my_api_key";
+
+    void validateClient(MandrillClient mandrillClient) {
+        assertNotNull(mandrillClient);
+        assertEquals(testApiKey, mandrillClient.getApiKey());
+    }
+
+    @Test
+    void getClientForApiKey() {
+        validateClient(MandrillClientFactory.getClientForApiKey(testApiKey));
+    }
+
+    @Test
+    void testGetClientForApiKeyTemplateProvided() {
+        validateClient(MandrillClientFactory.getClientForApiKey(testApiKey, mock(RestTemplate.class)));
+    }
+
+    @Test
+    void testGetClientForApiKeyTemplateSupplierProvided() {
+        validateClient(MandrillClientFactory.getClientForApiKey(testApiKey, () -> mock(RestTemplate.class)));
+    }
+
+    @Test
+    void testGetClientForApiKeyTemplateSupplierProvidesNull() {
+        assertThrows(NullPointerException.class, () -> MandrillClientFactory.getClientForApiKey(testApiKey, () -> null));
+    }
+}
