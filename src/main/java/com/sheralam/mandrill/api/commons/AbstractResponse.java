6fab057f65de9a5b5d41a85b935e15269aa167e9
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-10 23:37:26 +0600

    Implement Abstract Response

---
+++ src/main/java/com/sheralam/mandrill/api/commons/AbstractResponse.java
@@ -0,0 +1,13 @@
+package com.sheralam.mandrill.api.commons;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+public abstract class AbstractResponse implements Serializable {
+}
