10d2db7ae13d1df47cfef042fa1452eb83aaf5c7
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-11 22:14:40 +0600

    Implement Sending template service

--- src/main/java/com/sheralam/mandrill/api/messages/model/request/TemplatePayload.java
+++ src/main/java/com/sheralam/mandrill/api/messages/model/request/TemplatePayload.java
@@ -12,20 +12,15 @@ import java.io.Serializable;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-@NoArgsConstructor
-public class TemplatePayload extends AbstractMessageAndTemplatePayload implements Serializable {
+public class TemplatePayload extends MessagePayload implements Serializable {
 
     private final static long serialVersionUID = 629126036398002490L;
     private String templateName;
     private TemplateContent templateContent;
 
 
-    public TemplatePayload(String key) {
-        super(key);
-    }
-
-    public TemplatePayload(String key, String templateName, TemplateContent templateContent) {
-        super(key);
+    public TemplatePayload(String key, String templateName, TemplateContent templateContent, Message message) {
+        super(key, message);
         this.templateName = templateName;
         this.templateContent = templateContent;
     }
