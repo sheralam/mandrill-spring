0d793f83d667a6f328ebe16f8fd0695e57f1c9bc
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-26 00:45:15 +0600

    implement  Info messages endpoint

---
+++ src/main/java/com/sheralam/mandrill/api/messages/model/request/InfoPayload.java
@@ -0,0 +1,23 @@
+package com.sheralam.mandrill.api.messages.model.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sheralam.mandrill.api.commons.AbstractPayload;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+
+@EqualsAndHashCode(callSuper = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Slf4j
+public class InfoPayload extends AbstractPayload {
+    private static final long serialVersionUID = 6477194538028113616L;
+    public String id;
+
+    public InfoPayload(String key) {
+        super(key);
+    }
+}
