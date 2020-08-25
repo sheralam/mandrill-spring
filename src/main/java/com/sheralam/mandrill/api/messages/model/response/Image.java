e126f3a2c9836632c5b8993a87bab1eed3e2053f
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-26 01:03:59 +0600

    implement  Parse messages endpoint

---
+++ src/main/java/com/sheralam/mandrill/api/messages/model/response/Image.java
@@ -0,0 +1,17 @@
+package com.sheralam.mandrill.api.messages.model.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class Image implements Serializable {
+    private static final long serialVersionUID = -3359196818789701537L;
+    public String name;
+    public String type;
+    public String content;
+}
