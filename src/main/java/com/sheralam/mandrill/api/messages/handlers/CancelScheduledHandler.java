66b1f8a53537ad27c7f1c9d34443edf5eabc3d38
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-26 01:17:47 +0600

    implement  CancelScheduled messages endpoint

---
+++ src/main/java/com/sheralam/mandrill/api/messages/handlers/CancelScheduledHandler.java
@@ -0,0 +1,26 @@
+package com.sheralam.mandrill.api.messages.handlers;
+
+import com.sheralam.mandrill.api.commons.RequestHandler;
+import com.sheralam.mandrill.api.messages.model.request.CancelScheduledPayload;
+import com.sheralam.mandrill.api.messages.model.request.MessagePayload;
+import com.sheralam.mandrill.api.messages.model.response.CancelScheduledResponse;
+import lombok.NonNull;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.function.Supplier;
+
+public class CancelScheduledHandler extends RequestHandler<CancelScheduledPayload, CancelScheduledResponse> {
+    private static final String endpointSuffix = "/messages/cancel-scheduled.json";
+
+
+
+    public CancelScheduledHandler(@NonNull Supplier<RestTemplate> restTemplateSupplier) {
+        super(endpointSuffix, restTemplateSupplier);
+    }
+
+    @Override
+    protected Class<CancelScheduledResponse> getResponseClass() {
+        return CancelScheduledResponse.class;
+    }
+
+}
