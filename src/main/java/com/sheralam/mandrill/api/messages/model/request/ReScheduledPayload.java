a99c83cd00f25141584c297decf7e2981013f39f
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-26 01:21:28 +0600

    implement  ReScheduled messages endpoint

---
+++ src/main/java/com/sheralam/mandrill/api/messages/model/request/ReScheduledPayload.java
@@ -0,0 +1,24 @@
+package com.sheralam.mandrill.api.messages.model.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sheralam.mandrill.api.commons.AbstractPayload;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+
+@EqualsAndHashCode(callSuper = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Slf4j
+public class ReScheduledPayload extends AbstractPayload {
+    private static final long serialVersionUID = 821962812353991993L;
+    private String id;
+    public String sendAt;
+
+    public ReScheduledPayload(String key){
+        super(key);
+    }
+}
