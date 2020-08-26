50da0ef91eedf47aeac9039d7847a326ec319474
Sher E Alam Sajib <sajibcse@gmail.com>
2020-08-26 23:49:26 +0600

    add initial test cases for all messages services

--- src/test/java/com/sheralam/mandrill/api/messages/MessagesCallsServiceTest.java
+++ src/test/java/com/sheralam/mandrill/api/messages/MessagesCallsServiceTest.java
@@ -1,74 +1,76 @@
 package com.sheralam.mandrill.api.messages;
 
 import com.sheralam.mandrill.api.exceptions.MandrillCommunicationException;
-import com.sheralam.mandrill.api.messages.model.request.Message;
-import com.sheralam.mandrill.api.messages.model.request.TemplateContent;
-import com.sheralam.mandrill.client.MandrillClient;
+import com.sheralam.mandrill.api.messages.model.request.*;
 import com.sheralam.mandrill.client.MandrillClientFactory;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MessagesCallsServiceTest {
     private final String invalidApi = "invalid_api";
+    private MessagesCallsService messages;
+
+    @BeforeEach
+    void init() {
+        messages = MandrillClientFactory.getClientForApiKey("invalid_api").getMessages();
+    }
+
+
     @Test
     void send() {
-        MandrillClient clientForApiKey = MandrillClientFactory.getClientForApiKey(invalidApi);
-        Assertions.assertNotNull(clientForApiKey);
-        Assertions.assertNotNull(clientForApiKey.getMessages());
-        assertThrows(MandrillCommunicationException.class, () -> MandrillClientFactory.getClientForApiKey("invalid_api", () -> null).getMessages().send(new Message()));
+        assertThrows(MandrillCommunicationException.class, () -> messages.send(new Message()));
     }
 
     @Test
     void sendTemplate() {
-        MandrillClient clientForApiKey = MandrillClientFactory.getClientForApiKey(invalidApi);
-        Assertions.assertNotNull(clientForApiKey);
-        Assertions.assertNotNull(clientForApiKey.getMessages());
-        assertThrows(MandrillCommunicationException.class, () -> MandrillClientFactory.getClientForApiKey("invalid_api", () -> null).getMessages().sendTemplate("template_name", new TemplateContent("content_name", "content"),new Message()));
+        assertThrows(MandrillCommunicationException.class, () -> messages.sendTemplate("template_name", new TemplateContent("content_name", "content"), new Message()));
     }
 
-    @Test
-    void testSend() {
-    }
-
-    @Test
-    void testSendTemplate() {
-    }
 
     @Test
     void content() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.content("invalid_id"));
     }
 
     @Test
     void cancelScheduled() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.cancelScheduled("invalid_id"));
     }
 
     @Test
     void info() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.info("invalid_id"));
     }
 
     @Test
     void listScheduled() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.listScheduled("abc@example.bd"));
     }
 
     @Test
     void parse() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.parse("rawMessage"));
     }
 
     @Test
     void reschedule() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.reschedule("invalid_id", "invalid_date"));
     }
 
     @Test
     void search() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.search(new Search()));
     }
 
     @Test
     void searchTimeSeries() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.searchTimeSeries(new SearchTimeSeries()));
     }
 
     @Test
     void sendRaw() {
+        assertThrows(MandrillCommunicationException.class, () -> messages.sendRaw(new SendRaw()));
     }
 }
